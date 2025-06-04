37046610c604eb34050dbea71ea27d90d16025c3
tertsman <tertsman@gmail.com>
2025-06-04 08:01:34 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky",this.window.scrollY > 0);
+})
+
+
+let menu = document.querySelector('#menu-icon');
+let navmenu = document.querySelector('.navmenu');
+
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navmenu.classList.toggle('open');
+
+}
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navmenu.classList.remove('open');
+}
